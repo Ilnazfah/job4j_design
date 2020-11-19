bcaf62875a1fc0ef80a5e441fb45fe46614a57dc
Ilnazfah <ifs3112@gmail.com>
2020-11-19 23:52:18 +0300

    Update

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,15 +5,30 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
+    private int size = 0;
 
     public T poll() {
-        while (in != null) {
+        size--;
+        for (int i = 0; i < in.size(); i++) {
             out.push(in.pop());
         }
-        return null;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        SimpleQueue<String> s = new SimpleQueue<>();
+        s.push("1");
+        s.push("2");
+        s.push("3");
+        System.out.println(s.size);
     }
 }
