dff122e473023add01cd15f2742417b197bbe8fd
Ilnazfah <ifs3112@gmail.com>
2020-11-25 17:53:24 +0300

    Перевернуть связанный список [#316139]

--- chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private int size = 0;
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
@@ -16,7 +17,18 @@ public class ForwardLinked<T> implements Iterable<T> {
         while (tail.next != null) {
             tail = tail.next;
         }
+        size++;
         tail.next = node;
+    }
+
+    public void revert() {
+        Node<T> node = new Node<T>(head.value, null);
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+        head = node;
     }
 
     public void deleteFirst() {
@@ -24,6 +36,7 @@ public class ForwardLinked<T> implements Iterable<T> {
             throw new NoSuchElementException();
         }
         head = head.next;
+        size--;
     }
 
     public T deleteLast() {
@@ -43,7 +56,12 @@ public class ForwardLinked<T> implements Iterable<T> {
             elem = tail.next.value;
             tail.next = null;
         }
+        size--;
         return elem;
+    }
+
+    public int getSize() {
+        return size;
     }
 
     @Override
