ad8e276311f74289e357cabf71da5e1f9bd37c93
Ilnazfah <ifs3112@gmail.com>
2020-12-08 21:47:35 +0300

    Создать модель User [#316164]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -7,27 +7,21 @@ public class User {
     private int children;
     private Calendar birthday;
 
-    public String getName() {
-        return name;
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
     public int getChildren() {
         return children;
     }
 
-    public void setChildren(int children) {
-        this.children = children;
-    }
-
     public Calendar getBirthday() {
         return birthday;
-    }
-
-    public void setBirthday(Calendar birthday) {
-        this.birthday = birthday;
     }
 }
