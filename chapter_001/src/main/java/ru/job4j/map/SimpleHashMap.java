ce656ec8506d91390f90162ab76e4310729b9c7e
Ilnazfah <ifs3112@gmail.com>
2021-03-24 23:39:41 +0300

    Реализовать собственную структуру данных - HashMap [#316171]

--- chapter_001/src/main/java/ru/job4j/map/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -3,28 +3,66 @@ package ru.job4j.map;
 import java.util.Iterator;
 
 public class SimpleHashMap<K, V> implements Iterable<V> {
-    private Object[] hashTable = new Object[16];
+    private Object[] hashTable;
+    private int size = 0;
+    private int capacity = 16;
+
+    public SimpleHashMap() {
+        hashTable = new Object[capacity];
+    }
 
     boolean insert(K key, V value) {
-        int hash = key.hashCode();
-        Object o = new MapCont(hash, key, value);
-        int index = getIndex(key);
-        hashTable[index] = o;
-        return false;
+        checkTable();
+        boolean res = checkHash(key);
+        if (res) {
+            int hash = key.hashCode();
+            Object o = new MapCont(hash, key, value);
+            int index = getIndex(key);
+            hashTable[index] = o;
+            size++;
+        }
+        return res;
     }
 
     V get(K key) {
         V result = null;
-        //result = hashTable[getIndex(key)].getClass();
+        MapCont o = (MapCont) hashTable[getIndex(key)];
+        try {
+            result = o.getValue();
+        } catch (NullPointerException ignored) { }
         return result;
     }
 
     boolean delete(K key) {
+        if (!checkHash(key)) {
+            int index = getIndex(key);
+            hashTable[index] = null;
+            size--;
+            return true;
+        }
         return false;
+    }
+
+    private void checkTable() {
+        Object[] temp;
+        if (size > hashTable.length - 1) {
+            capacity *= 2;
+            temp = new Object[capacity];
+            System.arraycopy(hashTable, 0, temp, 0, size);
+            hashTable = temp;
+        }
     }
 
     int getIndex(K key) {
         return key.hashCode() & (hashTable.length - 1);
+    }
+
+    boolean checkHash(K key) {
+        return hashTable[getIndex(key)] == null;
+    }
+
+    public int getSize() {
+        return size;
     }
 
     @Override
@@ -43,8 +81,21 @@ public class SimpleHashMap<K, V> implements Iterable<V> {
             this.value = value;
         }
 
-        public V getValue () {
+        public V getValue() {
             return value;
         }
+    }
+
+    public static void main(String[] args) {
+        SimpleHashMap<String, String> simpleHashMap = new SimpleHashMap<>();
+        simpleHashMap.insert("Coffee", "Coffee");
+        System.out.println(simpleHashMap.insert("Tea", "Tea"));
+        System.out.println("Size " + simpleHashMap.getSize());
+
+        System.out.println(simpleHashMap.get("Coffee"));
+        System.out.println(simpleHashMap.get("Tea"));
+        System.out.println(simpleHashMap.delete("Tea"));
+        System.out.println("Size " + simpleHashMap.getSize());
+        System.out.println(simpleHashMap.get("Tea"));
     }
 }
