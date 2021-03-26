dedae755ed066f665d0712890f583f6e83245089
Ilnazfah <ifs3112@gmail.com>
2021-03-26 14:05:58 +0300

    Реализовать собственную структуру данных - HashMap [#316171]

--- chapter_001/src/main/java/ru/job4j/map/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -1,11 +1,14 @@
 package ru.job4j.map;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class SimpleHashMap<K, V> implements Iterable<V> {
     private Object[] hashTable;
     private int size = 0;
     private int capacity = 16;
+    private int modCount;
 
     public SimpleHashMap() {
         hashTable = new Object[capacity];
@@ -20,6 +23,7 @@ public class SimpleHashMap<K, V> implements Iterable<V> {
             int index = getIndex(key);
             hashTable[index] = o;
             size++;
+            modCount++;
         }
         return res;
     }
@@ -38,6 +42,7 @@ public class SimpleHashMap<K, V> implements Iterable<V> {
             int index = getIndex(key);
             hashTable[index] = null;
             size--;
+            modCount++;
             return true;
         }
         return false;
@@ -65,11 +70,6 @@ public class SimpleHashMap<K, V> implements Iterable<V> {
         return size;
     }
 
-    @Override
-    public Iterator<V> iterator() {
-        return null;
-    }
-
     class MapCont {
         private int hash;
         private K key;
@@ -86,16 +86,29 @@ public class SimpleHashMap<K, V> implements Iterable<V> {
         }
     }
 
-    public static void main(String[] args) {
-        SimpleHashMap<String, String> simpleHashMap = new SimpleHashMap<>();
-        simpleHashMap.insert("Coffee", "Coffee");
-        System.out.println(simpleHashMap.insert("Tea", "Tea"));
-        System.out.println("Size " + simpleHashMap.getSize());
+    @Override
+    public Iterator<V> iterator() {
+        return new SimpleHashMap.It();
+    }
 
-        System.out.println(simpleHashMap.get("Coffee"));
-        System.out.println(simpleHashMap.get("Tea"));
-        System.out.println(simpleHashMap.delete("Tea"));
-        System.out.println("Size " + simpleHashMap.getSize());
-        System.out.println(simpleHashMap.get("Tea"));
+    private class It implements Iterator<V> {
+        private int point = 0;
+        private int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return point < size;
+        }
+
+        @Override
+        public V next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            return (V) hashTable[point++];
+        }
     }
 }
--- chapter_001/src/test/java/ru/job4j/map/SimpleHashMapTest.java
+++ chapter_001/src/test/java/ru/job4j/map/SimpleHashMapTest.java
@@ -1,7 +1,49 @@
 package ru.job4j.map;
 
-import java.util.Iterator;
+import org.junit.Test;
 
-public class SimpleHashMapTest{
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 
+public class SimpleHashMapTest {
+    private SimpleHashMap<Integer, String> simpleHashMap = new SimpleHashMap<>();
+
+    @Test
+    public void whenInsertElement() {
+        simpleHashMap.insert(1, "Ilnaz1");
+        simpleHashMap.insert(2, "Ilnaz2");
+        int size = simpleHashMap.getSize();
+        assertThat(size, is(2));
+    }
+
+    @Test
+    public void whenInsertThenGet() {
+        simpleHashMap.insert(1, "Ilnaz1");
+        String value = simpleHashMap.get(1);
+        assertThat(value, is("Ilnaz1"));
+        simpleHashMap.insert(2, "Ilnaz2");
+        value = simpleHashMap.get(2);
+        assertThat(value, is("Ilnaz2"));
+    }
+
+    @Test
+    public void whenDelete() {
+        simpleHashMap.insert(1, "Ilnaz1");
+        simpleHashMap.insert(2, "Ilnaz2");
+        simpleHashMap.delete(1);
+        assertThat(simpleHashMap.get(1), nullValue());
+        assertThat(simpleHashMap.getSize(), is(1));
+    }
+
+    @Test
+    public void whenIterate() {
+        var iterator = simpleHashMap.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            i++;
+        }
+        assertThat(i, is(simpleHashMap.getSize()));
+    }
 }
