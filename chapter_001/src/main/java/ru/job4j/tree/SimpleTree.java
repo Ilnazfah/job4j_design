0ffc751f72a672552b245acf64d48cb3aacf2410
Ilnazfah <ifs3112@gmail.com>
2021-04-12 18:03:10 +0300

    Добавить метод boolean isBinary()

--- chapter_001/src/main/java/ru/job4j/tree/SimpleTree.java
+++ chapter_001/src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -23,12 +24,24 @@ class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl;
+        Predicate<Node<E>> predicate = val -> val.value.equals(value);
+        rsl = findByPredicate(predicate);
+        return rsl;
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> predicate = child -> !child.children.isEmpty() && child.children.size() > 2;
+        return findByPredicate(predicate).isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
+++ chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tree;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -13,19 +14,34 @@ import static org.junit.Assert.*;
         tree.add(1, 4);
         tree.add(4, 5);
         tree.add(5, 6);
-        assertThat(
-                tree.findBy(6).isPresent(),
-                is(true)
-        );
+        assertTrue(tree.findBy(6).isPresent());
     }
 
     @Test
     public void when6ElFindNotExitThenOptionEmpty() {
         Tree<Integer> tree = new SimpleTree<>(1);
         tree.add(1, 2);
-        assertThat(
-                tree.findBy(7).isPresent(),
-                is(false)
-        );
+        assertFalse(tree.findBy(7).isPresent());
     }
+
+        @Test
+        public void whenMore3Children() {
+            Tree<Integer> tree = new SimpleTree<>(1);
+            tree.add(1, 2);
+            tree.add(1, 3);
+            tree.add(1, 4);
+            tree.add(4, 5);
+            tree.add(5, 6);
+            Assert.assertFalse(tree.isBinary());
+        }
+
+        @Test
+        public void whenAll2Children() {
+            Tree<Integer> tree = new SimpleTree<>(1);
+            tree.add(1, 2);
+            tree.add(1, 3);
+            tree.add(4, 5);
+            tree.add(4, 6);
+            Assert.assertTrue(tree.isBinary());
+        }
 }
