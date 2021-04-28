fbb2cc49539493f4be9c9bff5b9eb2dfe2728bcc
Ilnazfah <ifs3112@gmail.com>
2021-04-28 17:56:12 +0300

    Анализ доступности сервера. [#316191]

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -1,22 +1,41 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            List<String> unavailable = new ArrayList<>();
+            String on = null;
+            String off = null;
+            while (in.readLine() != null) {
+                String line = in.readLine();
+                if (line.contains("400") || line.contains("500")) {
+                    on = line.substring(4, 12);
+                }
+                if (line.contains("200") || line.contains("300")) {
+                    off = line.substring(4, 12);
+                    unavailable.add(on + ";" + off);
+                }
+            }
+            writeFile(target, unavailable);
         } catch (Exception  e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
+    private void writeFile(String path, List<String> date) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(path))) {
+            date.forEach(out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("server.log", "target.csv");
     }
 }
