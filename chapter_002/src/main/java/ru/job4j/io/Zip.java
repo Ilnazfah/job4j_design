d3bb881499eb1ac7ca138582e4584c1c414e190f
Ilnazfah <ifs3112@gmail.com>
2021-05-21 00:15:03 +0300

    Архивировать проект [#316192]

--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -2,17 +2,20 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import static ru.job4j.io.Search.search;
+
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File source : sources) {
-                zip.putNextEntry(new ZipEntry(source.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getAbsolutePath().split(":\\\\")[1]));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile().getAbsolutePath()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -32,15 +35,17 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
-//        if (args.length == 0) {
-//            throw new IllegalArgumentException();
-//        }
-//        ArgsName ar = ArgsName.of(args);
-//        String directory = ar.get("d");
-//        String exclude  = ar.get("e");
-//        String output  = ar.get("o");
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        ArgsName ar = ArgsName.of(args);
+        String directory = ar.get("d");
+        String exclude  = ar.get("e");
+        String output  = ar.get("o");
+        Path start = Paths.get(directory);
+        List<Path> paths = search(start, p -> !p.toFile().getName().endsWith(exclude));
         Zip zip = new Zip();
-        zip.packSingleFile(new File("F:\\projects\\job4j_design\\chapter_002\\log.txt"), new File("F:\\projects\\job4j_design\\chapter_002\\log.zip"));
+        zip.packFiles(paths, new File(output));
     }
 }
