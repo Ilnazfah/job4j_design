b1720ee5d255f12bc4fa5847510abd50f59c1e9c
Ilnazfah <ifs3112@gmail.com>
2021-09-07 13:21:20 +0300

    Scanner

--- chapter_002/src/main/java/ru/job4j/io/CSVReader.java
+++ chapter_002/src/main/java/ru/job4j/io/CSVReader.java
@@ -1,26 +1,67 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class CSVReader {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException();
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
         ArgsName ar = ArgsName.of(args);
+        String path = ar.get("path");
+        String delimiter = ar.get("delimiter");
+        String out = ar.get("out");
+        String filter = ar.get("filter");
         CSVReader csvReader = new CSVReader();
-        csvReader.readFile("file.csv");
+        csvReader.run(path, delimiter, out, filter);
     }
 
-    public void readFile(String path) throws IOException {
-        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-            in.lines().forEach(System.out::println);
+    public void run(String path, String delimiter, String out, String filter) {
+        List<String> file = readFile(path, delimiter, filter);
+        if (out.equals("stdout")) {
+            for (String s : file) {
+                    System.out.println(s);
+            }
+        } else {
+            try (PrintWriter outFile = new PrintWriter(
+                    new BufferedOutputStream(new FileOutputStream(out)))) {
+                for (String s : file) {
+                    outFile.println(s);
+                }
+            } catch (Exception e) {
+                    e.printStackTrace();
+            }
         }
-//        Scanner scanner = new Scanner();
-//            while (scanner.hasNextInt()) {
-//                System.out.print(scanner.nextInt());
-//                System.out.print(" ");
-//            }
+    }
+
+    private List<String> readFile(String path, String delimiter, String filter) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            Scanner scan = new Scanner(in);
+                String[] filterArr = filter.split(",");
+                int[] indexes = null;
+                while (scan.hasNextLine()) {
+                    List<String> lineArr = Arrays.asList(scan.nextLine().split(delimiter));
+                    String line = "";
+                    if (indexes == null) {
+                        indexes = new int[filterArr.length];
+                        for (int i = 0; i < indexes.length; i++) {
+                            indexes[i] = lineArr.indexOf(filterArr[i]);
+                        }
+                    }
+                    for (int i = 0; i < filterArr.length; i++) {
+                        line += lineArr.get(indexes[i]) + " ";
+                    }
+                    result.add(line.strip());
+                }
+                scan.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
  }
