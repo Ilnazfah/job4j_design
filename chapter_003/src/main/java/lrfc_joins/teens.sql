01918c1ed0b9404c6e098a47c19d617cb5e2e061
Ilnazfah <ifs3112@gmail.com>
2021-09-27 19:10:29 +0300

    Join

--- chapter_003/src/main/java/lrfc_joins/teens.sql
+++ chapter_003/src/main/java/lrfc_joins/teens.sql
@@ -4,6 +4,9 @@ CREATE TABLE teens(
    gender varchar(255)
 );
 
+
+--5. Создать таблицу teens с атрибутами name, gender и заполнить ее.
+--Используя cross join составить все возможные разнополые пары
 INSERT INTO teens(name, gender) VALUES ('Олег', 'male');
 INSERT INTO teens(name, gender) VALUES ('Игорь', 'male');
 INSERT INTO teens(name, gender) VALUES ('Дмитрий', 'male');
@@ -11,8 +14,6 @@ INSERT INTO teens(name, gender) VALUES ('Наталья', 'female');
 INSERT INTO teens(name, gender) VALUES ('Юлия', 'female');
 INSERT INTO teens(name, gender) VALUES ('Оксана', 'female');
 
---5. Создать таблицу teens с атрибутами name, gender и заполнить ее.
---Используя cross join составить все возможные разнополые пары
 SELECT gm.name AS male, gf.name AS female,(gf.gender, gm.gender) AS Couple
 FROM teens AS gf CROSS JOIN teens AS gm
 WHERE gf.gender = 'female' AND  gm.gender = 'male';
